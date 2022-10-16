60d1f1df09542435d42a75392c67ff150841413e
KeziahF <keziah.furnell@my.jcu.edu.au>
2022-10-16 22:38:09 +1000

    completed emails - take inputs, update and display dictionary

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,16 +1,34 @@
-emails_to_name = {}
-user_email = input("Email: ").lower()
+"""
+CP1404 Prac
+Emails
+Estimate: 30 minutes
+Actual: 45 minutes
+"""
+def main(user_email):
+    email_to_name = {}
+    user_email = input("Email: ").lower()
+    while user_email != '':
+        if user_email in email_to_name.keys():
+            print("Email already in use")
+            user_email = input("Email: ").lower()
+        else:
+            extract_name(user_email, email_to_name)
 
-while user_email != '':
-    if user_email in emails_to_name.keys():
-        print("Email already in use")
         user_email = input("Email: ").lower()
-    else:
-        username = user_email.split('@')
-        name = ' '.join(map(str, username[0].split('.'))).title()
+
+    for pair in email_to_name.items():
+        print(f"{pair[1]}  ({pair[0]})")
+def extract_name(user_email, email_to_name):
+    yes_choice = ['yes', 'y']
+    no_choice = ['no', 'n']
+    username = user_email.split('@')
+    name = ' '.join(map(str, username[0].split('.'))).title()
+    correct_name = input(f"Is your name: {name}? (y/n) ").lower()
+    if correct_name in no_choice:
+        name = input("Name: ").lower().title()
+    email_to_name[user_email] = name
+    return email_to_name
 
 
-        print(f"Is your name: {name}?")
-        break
-
-print(emails_to_name)
+user_email = 'email'
+main(user_email)
