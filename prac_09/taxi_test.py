6f28c39ecf5d269734644942928f948715098db1
KeziahF <keziah.furnell@my.jcu.edu.au>
2022-11-22 10:08:26 +1100

    taxi test completed

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,16 @@
+from prac_09.taxi import Taxi
+
+def main():
+    taxi = Taxi("Prius 1", 100, 1.23)
+    taxi.drive(40)
+    print(taxi)
+    fare = taxi.get_fare()
+    print(f"${fare}")
+    taxi.start_fare()
+    taxi.drive(100)
+    print(taxi)
+    fare = taxi.get_fare()
+    print(f"${fare}")
+
+
+main()
